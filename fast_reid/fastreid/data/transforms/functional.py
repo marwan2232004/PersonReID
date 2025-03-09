283c648b656c1c2b18a2235d37419564e399104a
marwan2232004 <118024824+marwan2232004@users.noreply.github.com>
2025-03-10 00:21:16 +0200

    Fix dtype specification in ByteTensor conversion for improved image processing

--- fast_reid/fastreid/data/transforms/functional.py
+++ fast_reid/fastreid/data/transforms/functional.py
@@ -43,7 +43,7 @@ def to_tensor(pic):
     elif pic.mode == '1':
         img = 255 * torch.from_numpy(np.array(pic, np.uint8, copy=False))
     else:
-        img = torch.ByteTensor(torch.UntypedStorage.from_buffer(pic.tobytes()))
+        img = torch.ByteTensor(torch.UntypedStorage.from_buffer(pic.tobytes(), dtype=torch.uint8))
     # PIL image mode: L, LA, P, I, F, RGB, YCbCr, RGBA, CMYK
     if pic.mode == 'YCbCr':
         nchannel = 3
